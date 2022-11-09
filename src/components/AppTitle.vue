ba863c155202ddd5a2e9463580e4533389fa761c
ValentinaZussino <vzussino@protonmail.com>
2022-11-09 14:31:00 +0100

    agg appTitle vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+    data(){
+        return {
+        title: 'La mia prima app con Vite!'
+    }
+},
+}
+</script>
+
+<style lang="css">
+div {
+    width: 80%;
+    height: 50px;
+    background-color: black;
+    text-align: center;
+}
+h1 {
+  color: violet;
+}
+</style>
